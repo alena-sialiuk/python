93b4d3eaf2b83b79c46705545bab1204225cfe71
alena-sialiuk <asialiuk@gmail.com>
2019-08-12 20:40:15 +0300

    Lab3 - Task1

--- labs/Lab3_homework1.py
+++ labs/Lab3_homework1.py
@@ -1,5 +1,4 @@
-from datetime import date
-""" 
+"""
 Домашняя работа 3, задание 1 
 Вариант 10
 Создать класс Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
@@ -7,47 +6,47 @@ from datetime import date
 a)	список автомобилей заданной марки;
 b)	список автомобилей заданной модели, которые эксплуатируются больше n лет;
 """
+from datetime import date
 class Car:
-
      # id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
-    __car_id = ""
-    __car_marka = ""
-    __car_model = ""
-    __car_year = ""
-    __car_colour = ""
+    __id = ""
+    __marka = ""
+    __model = ""
+    __year = ""
+    __colour = ""
     __price = 0
     __reg_num = ""
 
    # конструктор с присвоением заданных значений
    # id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
-    def __init__(self, car_id, car_marka, car_model, car_year, car_colour, price, reg_num):
-        self.__car_id = car_id
-        self.__car_marka = car_marka
-        self.__car_model = car_model
-        self.__car_year = car_year
-        self.__car_colour = car_colour
+    def __init__(self, id, marka, model, year, colour, price, reg_num):
+        self.__id = id
+        self.__marka = marka
+        self.__model = model
+        self.__year = year
+        self.__colour = colour
         self.__price = price
         self.__reg_num = reg_num
 
     # чтение id car
     def get_car_id(self):
-        return self.__car_id
+        return self.__id
 
     # чтение марки car
     def get_car_marka(self):
-        return self.__car_marka
+        return self.__marka
 
     # чтение модели car
     def get_car_model(self):
-        return self.__car_model
+        return self.__model
 
     # чтение года выпуска car
     def get_car_year(self):
-        return self.__car_year
+        return self.__year
 
     # чтение цвета car
     def get_car_colour(self):
-        return self.__car_colour
+        return self.__colour
 
     # чтение цены car
     def get_price(self):
@@ -60,7 +59,7 @@ class Car:
     # получить возраст car
     def get_age_car(self):
         today = date.today()
-        return today.year - self.__car_year
+        return today.year - self.__year
 
     # напечатать информацию об car
     def car_print_info(self):
@@ -74,7 +73,7 @@ class Car:
               )
 # main
 # создать список объектов класса Car
-cars = [
+Cars = [
    Car(1, "Opel", "Signum", 2005, "grey", 5400, "2304-MP 5"),
    Car(2, "Opel", "Astra", 2010, "white", 6400, "2504-MP 7"),
    Car(3, "Opel", "Vectra", 2003, "brown", 3700, "4304-SR 5"),
@@ -88,24 +87,24 @@ cars = [
 ]
 print("Список всех автомобилей:")
 print("Id  Marka  Model  Year   Colour Price   Reg_num ")
-for i in range(len(cars)):
-    cars[i].car_print_info()
+for i in range(len(Cars)):
+    Cars[i].car_print_info()
 
 # вывести список автомобилей заданной марки;
 m = input("Введите марку автомобиля:")
 count = 0
-for i in range(len(cars)):
-    if str.capitalize(cars[i].get_car_marka()) == str.capitalize(m):
+for i in range(len(Cars)):
+    if str.capitalize(Cars[i].get_car_marka()) == str.capitalize(m):
        count += 1
 if count == 0:
-    m = print ("Такой марки нет в списке\n")
+    m = print ("Такой марки нет в базе\n")
 
 if count >0:
     print("Список всех автомобилей марки " + m + ":")
     print("Id   Marka   Model  Year   Colour    Price    Reg_num ")
-    for i in range(len(cars)):
-        if str.capitalize(cars[i].get_car_marka()) == str.capitalize(m):
-            cars[i].car_print_info()
+    for i in range(len(Cars)):
+        if str.capitalize(Cars[i].get_car_marka()) == str.capitalize(m):
+            Cars[i].car_print_info()
 
 # список автомобилей заданной модели, которые эксплуатируются больше n лет;
 m = input("Введите модель автомобиля: \n")
@@ -113,10 +112,10 @@ n = int(input("Введите возраст автомобиля: \n"))
 print("Список всех автомобилей модели " + str.capitalize(m) + " ,которые эксплуатируются больше " + str(n) + " лет:")
 print("Id   Marka   Model  Year   Colour    Price    Reg_num ")
 count = 0
-for i in range(len(cars)):
-    if str.capitalize(cars[i].get_car_model()) == str.capitalize(m):
-        if cars[i].get_age_car() >= n:
-            cars[i].car_print_info()
+for i in range(len(Cars)):
+    if str.capitalize(Cars[i].get_car_model()) == str.capitalize(m):
+        if Cars[i].get_age_car() >= n:
+            Cars[i].car_print_info()
             count += 1
 if count == 0:
     print("Таких автомобилей в базе нет!")
--- labs/Lab3_homework2.py
+++ labs/Lab3_homework2.py
@@ -1,8 +1,103 @@
 """
- Домашняя работа 3, задание 1
-5 Вариант 10
-Создать класс Car: id, Марка, Модель, Год выпуска, Цвет, Цена, Регистрационный номер.
-Создать список объектов. Вывести:
-a)	список автомобилей заданной марки;
-b)	список автомобилей заданной модели, которые эксплуатируются больше n лет;
+Домашняя работа 3, задание 2
+Задание наследование и полиморфизм
+ Вариант 9
+
 """
+from datetime import date
+
+
+# Класс Продукт
+class Product:
+    # наименовение
+    __name: str = ""
+
+    def __init__(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+
+# Класс Товар
+class Goods(Product):
+    # Стоимость
+    __price = 0
+
+    def __init__(self, name, price):
+        Product.__init__(self, name)
+        self.__price = price
+
+    def get_price(self):
+        return self.__price
+
+
+class Flowers(Goods):
+    # Длина
+    __length = 0
+    # Цвет
+    __color = ""
+
+    def __init__(self, name, price, color, length):
+        Goods.__init__(self, name, price)
+        self.__color = color
+        self.__length = length
+
+    def get_length(self):
+        return self.__length
+
+    def get_color(self):
+        return self.__color
+
+
+class Clock(Product):
+    # Тип часов
+    __type = ""
+
+    def __init__(self, name, price, type):
+        Goods.__init__(self, name, price)
+        self.__type = type
+
+
+# Класс пищевые товары
+class Food(Goods):
+    # Срок годности
+    __bestbefore = date.today()
+    # Калорийность
+    __energy = 0
+
+    def __init__(self, name, price, bestbefore, energy):
+        Goods.__init__(self, name, price)
+        self.__energy = energy
+        self.__bestBefore = bestbefore
+
+    def get_energy(self):
+        return self.__energy
+
+    def get_bestbefore(self):
+        return self.__bestbefore
+
+
+# Класс Торты
+class Cake(Food):
+    # Масса
+    __weight = 0
+
+    def __init__(self, name, price, weight, bestbefore, energy):
+        Food.__init__(self, name, price, bestbefore, energy)
+        self.__weight = weight
+
+    def get_weight(self):
+        return self.__weight
+
+
+class Candy(Food):
+    # Начинка
+    __filling = ""
+
+    def __init__(self, name, price, filling, bestbefore, energy):
+        Food.__init__(self, name, price, bestbefore, energy)
+        self.__filling = filling
+
+    def get_filling(self):
+        return self.__filling
